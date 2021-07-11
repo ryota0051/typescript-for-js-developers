7dd1e4dda538333b119ec24aeb7124cb54fe783d
ryota0051 <re0061vh@gmail.com>
2021-07-11 12:43:56 +0900

    add arrow function version overload

--- src/overloads.ts
+++ src/overloads.ts
@@ -1,11 +1,23 @@
 export {};
 
+// 名前あり関数の場合
 function double(value: number): number;
 function double(value: string): string;
 
 function double(value: any): any {
   return typeof value === 'number' ? value * 2 : value + value;
 }
+
+// アロー関数の場合
+
+// type Double = {
+//   (value: number): number;
+//   (value: string): string;
+// };
+
+// let double: Double = (value: any): any => {
+//   return typeof value === 'number' ? value * 2 : value + value;
+// };
 
 console.log(double(100));
 console.log(double('GO '));
